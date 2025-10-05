120071a37a1d3bd8712c8b325b222680bfbca1dd
Kiyan Pourazar <74766125+KiyanPourazar@users.noreply.github.com>
2025-10-05 14:36:37 +0330

    add about and pages links

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductsView
+from .views import ProductsView, about
 
 urlpatterns = [
     path('', ProductsView.as_view(), name='products'),
+    path('about/', about, name='about'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -6,3 +6,6 @@ class ProductsView(View):
     def get(self, request):
         all_products = Product.objects.all()
         return render(request, 'index.html', {"all_products": all_products})
+
+def about(request):
+    return render(request, 'about.html')
