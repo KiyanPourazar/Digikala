91130fef9929f8298be29cb0ed63ae8ea46f41f7
Kiyan Pourazar <74766125+KiyanPourazar@users.noreply.github.com>
2025-10-07 12:02:39 +0330

    add login and logout pages

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ProductsView, about
+from .views import ProductsView, about, login_user, logout_user, register_user
 
 urlpatterns = [
     path('', ProductsView.as_view(), name='products'),
     path('about/', about, name='about'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('register/',register_user,name='register'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,10 @@
+from urllib import request
+
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login , logout
+from django.contrib import messages
 
 class ProductsView(View):
     def get(self, request):
@@ -9,3 +13,34 @@ class ProductsView(View):
 
 def about(request):
     return render(request, 'about.html')
+
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('login_field')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'با موفقیت وارد شدید!')
+            return redirect('products')
+        else:
+            messages.error(request, 'نام کاربری یا رمز عبور اشتباه است!')
+            return redirect('login')
+
+    return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'با موفقیت خارج شدید!')
+    return redirect('products')
+
+def register_user(request):
+    return render(request, 'register.html')
